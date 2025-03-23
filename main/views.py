9b7e2708cfd1bd83c20df3dd56687b9508f568d6
madeinic <madecr28@gmail.com>
2025-03-23 16:51:22 -0600

    sweet alerts

--- main/views.py
+++ main/views.py
@@ -57,6 +57,7 @@ def signin_view(request):
 
 def logout_view(request):
     logout(request)
+    messages.success(request, 'Has cerrado sesión correctamente')
     return redirect('signin')
 
 def signup_view(request):
