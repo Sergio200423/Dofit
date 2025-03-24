144ddb32b6d0decaa96d3fddd01f7f175ccc4f51
Sergio200423 <sergiodanielxd2004@gmail.com>
2025-03-24 07:33:46 -0600

    Eliminar signup, Agregar directorio utils, GenerarCodigoAleatorio , y modificar vistas, email_enviado, y signin

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin_view, name='signin'),
-    path('signup/', views.signup_view, name='signup'),
     path('clientes/', views.clientes_view, name='clientes'),
     path('registro_clientes/', views.registro_clientes_view, name='registro_clientes'),
     path('asistencia/', views.asistencia_view, name='asistencia'),
