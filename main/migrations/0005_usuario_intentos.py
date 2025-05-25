b7effcbd389346e612dd87764d6c240690cf3774
Sergio200423 <sergiodanielxd2004@gmail.com>
2025-05-24 22:07:41 -0600

    mejorando las migraciones

--- main/migrations/0005_usuario_intentos.py
+++ main/migrations/0005_usuario_intentos.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0004_tipos_membresia'),
+        ('main', '0001_initial'),
     ]
 
     operations = [
